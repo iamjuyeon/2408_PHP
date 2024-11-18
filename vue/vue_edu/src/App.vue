3e599d44a22baef598d5308238b01ab44f3cdfe1
iamjuyeon <juyoon57@naver.com>
2024-11-18 18:11:51 +0900

    20241118 뷰.js 시작

---
+++ vue/vue_edu/src/App.vue
@@ -0,0 +1,167 @@
+<template> 
+<!-- html -->
+
+<!-- 자식 컴포넌트 호출 -->
+<BoardComponent />
+
+
+
+
+<hr>
+<!-- 리스트 렌더링 -->
+<!-- v-for -->
+<!-- 단순 반목 -->
+<div v-for="val in 5" :key="val">
+    <p>{{ val }}</p>
+</div>
+<hr>
+
+<!-- 별 찍기? -->
+<div v-for="star in 6" :key="star">
+    <p>*****</p>
+</div>
+
+
+<!-- 구구단 -->
+<!-- <div v-for="x in 9" :key="x++">
+    <p>🍅🍅🍅{{ x }}단🍅🍅🍅</p>
+    <div v-for="y in 9" :key="y">
+        <p>{{ x }} X {{ y }} ={{ x*y }} </p>
+    </div>
+</div> -->
+<hr>
+
+<!-- foreach -->
+<div v-for="(item, key) in data" :key="item">
+<p>{{ `${key}번째 ${item.name} - ${item.age} - ${item.gender}` }}</p>
+</div>
+<button @click="data.pop">둘리삭제</button>
+
+
+<!-- v-if -->
+<h1 v-if="cnt >= 5">5보다 크다</h1>
+<h1 v-if="cnt === 7">러키비키자냐⭐⭐</h1>
+<h1 v-else-if="cnt < 0">음수입니다</h1>
+<h1 v-else>나머지</h1>
+
+<button @click="addCnt">증가</button>
+<button @click="disCnt">감소</button>
+<hr>
+
+
+<!-- v-show -->
+<h1 v-setInterval="flgshow">브이쇼</h1>
+<button @click="flgshow = !flgshow">브이쇼 토글</button>
+
+
+
+
+
+<!-- reactive -->
+<hr>
+<h2>*이름 : {{ userInfo.name }}</h2>
+<h2 :class="ageBlue">*나이 : {{ userInfo.age }}</h2>
+<h2>*성별 : {{ userInfo.gender }}</h2>
+<button @click="changeName">이름 변경</button>
+<button @click="changeAgeBlue">나이 색깔 변경</button>
+<hr>
+
+<input type="text" v-model="transgender"> 
+<button @click="userInfo.gender = transgender">성별 변경</button>
+<p>{{  transgender }}</p>
+
+
+
+</template>
+
+
+
+
+
+<script setup> //javascript
+import BoardComponent from './components/BoardComponent.vue';
+
+
+import { reactive, ref } from 'vue';
+
+
+// -------foreach---------
+const data = reactive(
+    [
+        {name : '홍길동', age : 20, gender : '남자'}
+        ,{name : '희동이', age : 12, gender : '여자'}
+        ,{name : '둘리', age : 40, gender : '수컷'}
+    ]);
+
+
+
+
+// setInterval
+(() => {
+    setInterval(() => {
+        flgshow.value = !flgshow.value
+    }, 500);
+});
+
+
+
+//---------v-show---------
+const flgshow = ref(true);
+
+
+//-------성별 바꾸기 input -------
+const transgender = ref('');
+
+
+
+//---------특정 객체 색깔 변경---------
+const ageBlue = ref('');
+function changeAgeBlue() {
+    ageBlue.value = 'blue';
+}
+
+// -------------reactive-------------
+const cnt = ref(0); //ref 객체, 객체의 value 프로퍼티에 저장되어 있음
+const userInfo = reactive({
+    name: '임꺽정'
+    ,age: 20
+    ,gender:'남자'
+})
+
+function changeName() {
+    userInfo.name = '갑순이';
+}
+
+//-------------ref-------------
+const addCnt = () => {
+    cnt.value++; // ref 객체의 value 프로퍼티에 접근
+}
+
+const disCnt = () => {
+    cnt.value--;
+}
+
+
+</script>
+
+
+
+
+<style>
+/* css */
+ #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.blue {
+    color : #0000ff;
+}
+
+
+
+</style>
---
+++ vue/vue_edu/src/components/BoardComponent.vue
@@ -0,0 +1,68 @@
+<template>
+<hr>
+<p>component------보드</p>
+
+<!-- method 핸들러 -->
+<p>버튼</p>
+<button @click="addCnt">{{ cnt }}</button>
+<br>
+<!-- 더쉽게(인라인 핸들러, 현업에서 잘 사용 안함) -->
+<p>마우스 엔터</p>
+<h1 @mouseenter="mouse++">{{ mouse }}</h1>
+
+<hr>
+<!-- 문자 깜빡깜박 setInterval -->
+<h1>문자가 깜빡깜박</h1>
+
+<hr>
+
+<!-- watchers -->
+<p>전화번호를 입력하세요</p>
+<input type="text" v-model="tel"> 
+
+<!-- 양식에 맞으면 o, 틀리면 x -->
+<span :style="color">{{ msg }}</span>
+
+
+</template>
+
+
+
+<script setup>
+import { ref, watch } from 'vue';
+
+// *******watchser*********
+const tel = ref('');
+const msg = ref('X');
+const color = ref('color: red;');
+
+
+watch(tel, (val) => { //tel의 변화를 감지하고 있다가, 변화를 감지하면 val이 tel의 값을 가져옴
+    const regex = /^[0-9]{10, 11}$/;
+    if(!regex.test(val)) {
+        msg.value = 'X';
+        color.value = 'color: red;';
+    } else {
+        msg.value = 'O';
+        color.value = 'color: blue;';
+    }
+});
+
+
+
+const mouse = ref(0);
+const cnt = ref(0);
+const addCnt = () => {
+    cnt.value++;
+}
+
+
+</script>
+
+
+
+
+<style>
+
+
+</style>
